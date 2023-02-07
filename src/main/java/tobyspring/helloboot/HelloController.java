592d8bcb3b08c6c2bdf438e75481281e37d126c3
hjun0917 <hjun0917@naver.com>
2023-02-07 18:54:59 +0900

    의존 오브젝트 추가 및 controller, service 분리

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,8 +1,13 @@
 package tobyspring.helloboot;
 
 
+import java.util.Objects;
+
 public class HelloController {
     public String hello(String name) {
-        return "Hello " + name;
+        SimpleHelloService helloService = new SimpleHelloService();
+
+        // Object 타입으로 인자를 체크(?)
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
