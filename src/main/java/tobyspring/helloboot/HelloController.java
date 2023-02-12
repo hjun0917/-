590607a63bf62ac22097307bc2184cfc59e14a5a
hjun0917 <hjun0917@naver.com>
2023-02-12 23:29:31 +0900

    - DispatcherServlet 전환 - 애노테이션 매핑 정보 사용 - 스프링 컨테이너로 통합

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,24 +1,23 @@
 package tobyspring.helloboot;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+@RequestMapping("/hello")
 public class HelloController {
-    /*
-    인터페이스를 필드에 변수로 생성
-    -> 모든 구현 클래스로 초기화 될 수 있음.
-    기존 코드로 SimpleHelloService 에 강하게 의존하고 있었지만
-    아래와 같은 코드로 변경을 통해 의존성을 감소(?)시킴
-     */
     private final HelloService helloService;
 
     public HelloController(HelloService helloService) {
         this.helloService = helloService;
     }
 
+    @GetMapping
+    @ResponseBody
     public String hello(String name) {
-
-        // Object 타입으로 인자를 체크(?)
         return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -2,57 +2,28 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class HellobootApplication {
 
     public static void main(String[] args) {
-        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        // Servlet 컨테이너를 만드는 생성 함수
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            GenericApplicationContext applicationContext = new GenericApplicationContext();
-            /*
-            interface 의 구현체를 bean 으로 등록
-            spring container 의 assembler 가 해당 interface 의 구현체를 필요로 하는 곳에 주입
-            순서는 상관 x, spring container 가 알아서 관리해준다.
-             */
-            applicationContext.registerBean(HelloController.class);
-            applicationContext.registerBean(SimpleHelloService.class);
-            applicationContext.refresh();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
 
-
-            servletContext.addServlet("frontController", new HttpServlet() {
-                @Override
-                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-                        /*
-                        바인딩 : 요청 파라미터로 넘어온 값을 자바에서 사용하는 타입의 값으로 묶어(?)주는 것 -> 23/02/05 표현이 올바른지 모르겠음
-                        자바 메서드를 호출할 때 사용될 인자 값으로 사용
-                         */
-                        String name = req.getParameter("name");
-
-                        HelloController helloController = applicationContext.getBean(HelloController.class);
-                        String ret = helloController.hello(name);
-
-                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println(ret);
-                    } else {
-                        // 모든 조건에 부합하지 않는 요청은 404 반환을 하기  위해
-                        resp.setStatus(HttpStatus.NOT_FOUND.value());
-                    }
-                }
-                // frontController 는 중앙화된 처리를 하기 위해 모든 요청을 다 받아야 한다.
-            }).addMapping("/*");
-        });
-        webServer.start();
+                TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet",
+                            new DispatcherServlet(this)
+                    ).addMapping("/*");
+                });
+                webServer.start();
+            }
+        };
+        applicationContext.registerBean(HelloController.class);
+        applicationContext.registerBean(SimpleHelloService.class);
+        applicationContext.refresh();
     }
 }
