f6e76d2933fd2f1e83c86e598fb6eb90d9f539ea
hjun0917 <hjun0917@naver.com>
2023-02-14 23:55:49 +0900

    Bean의 생명주기 메소드

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -2,6 +2,8 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -10,6 +12,18 @@ import org.springframework.web.servlet.DispatcherServlet;
 @Configuration
 @ComponentScan
 public class HellobootApplication {
+    // <팩토리 메서드> 를 이용하여 두 개의 오베젝트를 빈으로 등록
+    // 서블릿펙토리와 디스패쳐서블릿을 빈으로 관리함으로써
+    // 조금 더 유연한 Spring Boot 애플리케이션을 구성할 수 있다.
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
 
     public static void main(String[] args) {
         AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
@@ -17,11 +31,15 @@ public class HellobootApplication {
             protected void onRefresh() {
                 super.onRefresh();
 
-                TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+                //스프링 컨테이너가 디스패쳐서브릿이 애플리케이션 컨텍스트가 필요함을
+                //인식하고 주입해준다.
+                //dispatcherServlet.setApplicationContext(this);
+
                 WebServer webServer = serverFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet",
-                            new DispatcherServlet(this)
-                    ).addMapping("/*");
+                    servletContext.addServlet("dispatcherServlet", dispatcherServlet)
+                            .addMapping("/*");
                 });
                 webServer.start();
             }
